e0d8429fa6fe6e0d1ed5c7ff5796fe489b30aece
yks218 <yks221188@163.com>
2026-07-23 19:20:55 +0800

    Delete Polarization-Aware Object Detection and Tracking System directory

--- Polarization-Aware Object Detection and Tracking System/export_onnx.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO(r"data/yolov8cbam_xyd/weights/best.pt")
-
-model.export(format="onnx", opset=12, dynamic=True)
--- Polarization-Aware Object Detection and Tracking System/ui-main.py
+++
@@ -1,8 +0,0 @@
-from ui import ReaderUI
-
-def main():
-    GUI = ReaderUI()
-    GUI.mainWindow.mainloop()
-
-if __name__ == '__main__':
-    main()
